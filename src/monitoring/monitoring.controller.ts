7ec69c3eb7eac93f7ee2360debe74b5dcfbcd838
RGNGIT <vnifedow@bk.ru>
2023-11-03 22:28:12 +0500

    Comments

--- src/monitoring/monitoring.controller.ts
+++ src/monitoring/monitoring.controller.ts
@@ -4,23 +4,23 @@ import { MonitoringService } from "./monitoring.service";
 @Controller()
 export class MonitoringController {
   constructor(private readonly monitoringService: MonitoringService) { }
-
+  // Фул постгре репорты по всем хостам юзера, к оторым подъебан юзер (таблица connections)
   @Get('fullReports/:tgId')
   async fullReportList(@Param('tgId') tgId) {
     const credStrings = await this.monitoringService.getPostgreCredsByTgId(tgId);
     return this.monitoringService.collectDatabaseFullInfos(credStrings);
   }
-
+  // Hепорты онли по БД по всем хостам юзера, к оторым подъебан юзер (таблица connections)
   @Get('fullHostsDbList/:tgId')
   async databaseList(@Param('tgId') tgId) {
     const credStrings = await this.monitoringService.getPostgreCredsByTgId(tgId);
     return this.monitoringService.collectDatabaseShortInfos(credStrings);
   }
-
+  // Все хосты юзера из таблицы connections
   @Get('usersHosts/:tgId')
   async hostsList(@Param('tgId') tgId) {
     const credStrings = await this.monitoringService.getPostgreCredsByTgId(tgId);
     return credStrings.map(cs => ({id: cs.id, host: cs.connectionString.split(';')[0]}));
   }
 
-}
+} 
