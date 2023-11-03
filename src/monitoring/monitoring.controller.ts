d95d12b6a4edfa5e22e56cfe48acde8b6ae355ae
Julia <74661905+SunDust23@users.noreply.github.com>
2023-11-03 23:49:13 +0500

    сваггер

--- src/monitoring/monitoring.controller.ts
+++ src/monitoring/monitoring.controller.ts
@@ -1,24 +1,30 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { MonitoringService } from "./monitoring.service";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
 @ApiTags('Мониторинг баз')
 @Controller()
 export class MonitoringController {
   constructor(private readonly monitoringService: MonitoringService) { }
-  // Фул постгре репорты по всем хостам юзера, к оторым подъебан юзер (таблица connections)
+  // Фул постгре репорты по всем хостам юзера, к которым подъебан юзер (таблица connections)
+  @ApiOperation({summary:'Полные отчеты по всем подключениям пользователя'})
+  @ApiResponse({status:200})
   @Get('fullReports/:tgId')
   async fullReportList(@Param('tgId') tgId) {
     const credStrings = await this.monitoringService.getPostgreCredsByTgId(tgId);
     return this.monitoringService.collectDatabaseFullInfos(credStrings);
   }
   // Hепорты онли по БД по всем хостам юзера, к оторым подъебан юзер (таблица connections)
+  @ApiOperation({summary:'Отчеты по базам данных по всем подключениям пользователя'})
+  @ApiResponse({status:200})
   @Get('fullHostsDbList/:tgId')
   async databaseList(@Param('tgId') tgId) {
     const credStrings = await this.monitoringService.getPostgreCredsByTgId(tgId);
     return this.monitoringService.collectDatabaseShortInfos(credStrings);
   }
   // Все хосты юзера из таблицы connections
+  @ApiOperation({summary:'Все подключения пользователя'})
+  @ApiResponse({status:200})
   @Get('usersHosts/:tgId')
   async hostsList(@Param('tgId') tgId) {
     const credStrings = await this.monitoringService.getPostgreCredsByTgId(tgId);
